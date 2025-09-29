3fed49f90295321208d807cd6cf6fb4cebc5c120
Eakansh Kumar <eakansh9927@gmail.com>
2025-09-29 19:39:47 +0530

    vite error resovled

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // ensures correct paths on Vercel
+  base: './', // use relative paths so JS/CSS load correctly on Vercel
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+  },
 });
