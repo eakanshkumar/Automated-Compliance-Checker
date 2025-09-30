b3a34fd26c7cc6ed6b1bc5065ae174952fda0237
Eakansh Kumar <eakansh9927@gmail.com>
2025-09-30 21:32:17 +0530

    localhost added in backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL from .env - fixed for both dev and prod
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL ="http://localhost:5000/api" || import.meta.env.VITE_API_URL;
 
 console.log('API Base URL:', API_BASE_URL); // Debug log
 
